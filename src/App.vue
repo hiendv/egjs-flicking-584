59e2071027b608264544e0f26efc07d7aed0bf0c
hiendv <hien.dao@icetea.io>
2021-11-11 15:18:42 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Flicking from '@egjs/vue3-flicking'
+import '@egjs/vue3-flicking/dist/flicking.css'
+
+const options = { align: 'prev', circular: true }
+</script>
+
+<template>
+  <Flicking
+    :options="options"
+    class="transform -skew-y-12 rotate-12"
+  >
+    <div style="padding: 1em; width: 50%; background-color:#ddd;">1</div>
+    <div style="padding: 1em; width: 50%; background-color:#aaa;">2</div>
+    <div style="padding: 1em; width: 50%; background-color:#ddd;">3</div>
+  </Flicking>
+</template>
